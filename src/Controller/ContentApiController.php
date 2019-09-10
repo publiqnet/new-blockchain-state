a42fc8e727a4b715b693213f771bdfcb5baeba14
Grigor Igitbashyan <grigor@arattauna.com>
2019-09-10 15:19:54 +0400

    Return duplicate URI error on ContentUnit submit

--- src/Controller/ContentApiController.php
+++ src/Controller/ContentApiController.php
@@ -209,7 +209,9 @@ class ContentApiController extends Controller
 
             //  Broadcast
             $broadcastResult = $blockChain->broadcast($signatureResult['transaction'], $publicKey, $signedContentUnit);
-            if (!($broadcastResult instanceof Done)) {
+            if ($broadcastResult instanceof UriError && $broadcastResult->getUriProblemType() == UriProblemType::duplicate) {
+                return new JsonResponse(['type' => 'duplicate_uri'], Response::HTTP_CONFLICT);
+            } elseif (!($broadcastResult instanceof Done)) {
                 throw new Exception('Broadcasting failed for URI: ' . $uri . '; Error type: ' . get_class($broadcastResult));
             }
 
