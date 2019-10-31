4157ea06931a2fad959655ef77816f3935b003fb
Grigor Igitbashyan <grigor@arattauna.com>
2019-10-31 17:53:29 +0400

    Minor change in Admin

--- src/Admin/PublicationAdmin.php
+++ src/Admin/PublicationAdmin.php
@@ -39,7 +39,7 @@ class PublicationAdmin extends AbstractAdmin
         $listMapper
             ->add('slug', null, ['header_style' => 'width: 300px'])
             ->add('title')
-            ->add('description')
+            ->add('description', null, ['collapse' => true])
             ->add('members', null, ['header_style' => 'width: 300px', 'label' => 'Members', 'template' => 'admin/publication_owner.html.twig'])
             ->add('subscribers', null, ['header_style' => 'width: 150px', 'label' => 'Subscribers', 'template' => 'admin/publication_subscribers_count.html.twig'])
             ->add('contentUnits', null, ['header_style' => 'width: 150px', 'label' => 'Articles', 'template' => 'admin/publication_articles_count.html.twig'])
