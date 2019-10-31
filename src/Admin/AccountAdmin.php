ad102dd82768b0687faa0381b6c1be48322ba07b
Grigor Igitbashyan <grigor@arattauna.com>
2019-10-31 17:49:33 +0400

    User / Publication data in Admin

---
+++ src/Admin/AccountAdmin.php
@@ -0,0 +1,53 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Grigor
+ * Date: 10/31/19
+ * Time: 2:09 PM
+ */
+
+namespace App\Admin;
+
+use Sonata\AdminBundle\Admin\AbstractAdmin;
+use Sonata\AdminBundle\Datagrid\ListMapper;
+use Sonata\AdminBundle\Datagrid\DatagridMapper;
+use Sonata\AdminBundle\Route\RouteCollection;
+
+class AccountAdmin extends AbstractAdmin
+{
+    protected $datagridValues = [
+        '_sort_order' => 'DESC',
+        '_sort_by' => 'id',
+    ];
+
+    protected function configureRoutes(RouteCollection $collection)
+    {
+        $collection->remove('delete');
+        $collection->remove('edit');
+        $collection->remove('create');
+    }
+
+    protected function configureDatagridFilters(DatagridMapper $datagridMapper)
+    {
+        $datagridMapper
+            ->add('publicKey')
+            ->add('email')
+            ->add('channel')
+            ->add('storage')
+        ;
+    }
+
+    protected function configureListFields(ListMapper $listMapper)
+    {
+        $listMapper
+            ->addIdentifier('publicKey', null, ['label' => 'Key', 'header_style' => 'width: 300px'])
+            ->add('email')
+            ->add('firstName')
+            ->add('lastName')
+            ->add('whole', null, ['label' => 'Balance', 'template' => 'admin/account_balance.html.twig'])
+            ->add('authorContentUnits', null, ['label' => 'Articles', 'template' => 'admin/account_articles_count.html.twig'])
+            ->add('channel')
+            ->add('storage')
+        ;
+    }
+}
---
+++ src/Admin/PublicationAdmin.php
@@ -0,0 +1,48 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Grigor
+ * Date: 10/31/19
+ * Time: 5:25 PM
+ */
+
+namespace App\Admin;
+
+use Sonata\AdminBundle\Admin\AbstractAdmin;
+use Sonata\AdminBundle\Datagrid\ListMapper;
+use Sonata\AdminBundle\Datagrid\DatagridMapper;
+use Sonata\AdminBundle\Route\RouteCollection;
+
+class PublicationAdmin extends AbstractAdmin
+{
+    protected $datagridValues = [
+        '_sort_order' => 'DESC',
+        '_sort_by' => 'id',
+    ];
+
+    protected function configureRoutes(RouteCollection $collection)
+    {
+        $collection->remove('delete');
+        $collection->remove('edit');
+        $collection->remove('create');
+    }
+
+    protected function configureDatagridFilters(DatagridMapper $datagridMapper)
+    {
+        $datagridMapper
+            ->add('slug')
+        ;
+    }
+
+    protected function configureListFields(ListMapper $listMapper)
+    {
+        $listMapper
+            ->add('slug', null, ['header_style' => 'width: 300px'])
+            ->add('title')
+            ->add('description')
+            ->add('members', null, ['header_style' => 'width: 300px', 'label' => 'Members', 'template' => 'admin/publication_owner.html.twig'])
+            ->add('subscribers', null, ['header_style' => 'width: 150px', 'label' => 'Subscribers', 'template' => 'admin/publication_subscribers_count.html.twig'])
+            ->add('contentUnits', null, ['header_style' => 'width: 150px', 'label' => 'Articles', 'template' => 'admin/publication_articles_count.html.twig'])
+        ;
+    }
+}
