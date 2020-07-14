a66bdefe06d3ab87c60b1f8c21c073eda911a3b1
Grigor Igitbashyan <grigor@arattauna.com>
2020-07-14 17:50:53 +0400

    Show last synced block data in Stats

--- src/Controller/Admin/StatsController.php
+++ src/Controller/Admin/StatsController.php
@@ -9,6 +9,7 @@
 namespace App\Controller\Admin;
 
 use App\Entity\Account;
+use App\Entity\Block;
 use App\Entity\ContentUnit;
 use App\Entity\Publication;
 use Doctrine\ORM\EntityManager;
@@ -19,6 +20,7 @@ class StatsController extends CRUDController
     /**
      * @return \Symfony\Component\HttpFoundation\Response
      * @throws \Symfony\Component\Serializer\Exception\ExceptionInterface
+     * @throws \Doctrine\ORM\NonUniqueResultException
      */
     public function listAction()
     {
@@ -33,10 +35,12 @@ class StatsController extends CRUDController
         $popularAuthorsViews = $em->getRepository(Account::class)->getPopularAuthors(10);
         $popularAuthorsArticles = $em->getRepository(Account::class)->getPopularAuthors(10, null, 'totalArticles');
         $authorsSummary = $em->getRepository(Account::class)->getAuthorsCount();
+        $lastBlock = $em->getRepository(Block::class)->getLastBlock();
 
         $popularAuthorsViews = $this->get('serializer')->normalize($popularAuthorsViews, null, ['groups' => ['accountBase', 'accountEmail', 'accountStats']]);
         $popularAuthorsArticles = $this->get('serializer')->normalize($popularAuthorsArticles, null, ['groups' => ['accountBase', 'accountEmail', 'accountStats']]);
         $popularPublications = $this->get('serializer')->normalize($popularPublications, null, ['groups' => ['publication', 'publicationStats']]);
+        $lastBlock = $this->get('serializer')->normalize($lastBlock, null, ['groups' => ['block']]);
 
         return $this->render('admin/stats.html.twig', [
             'publicationsSummary' => $publicationsSummary[0],
@@ -45,6 +49,7 @@ class StatsController extends CRUDController
             'popularAuthorsViews' => $popularAuthorsViews,
             'popularAuthorsArticles' => $popularAuthorsArticles,
             'popularPublications' => $popularPublications,
+            'lastBlock' => $lastBlock,
         ]);
     }
 }
